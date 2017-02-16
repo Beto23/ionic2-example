ccc3c22f00bff80f1a8aaeaede06ecfb51577310
beto8 <alberto@collectivemedia.mx>
2017-02-16 11:56:46 -0600

    Add list favoritos

--- src/pages/my-teams/my-teams.ts
+++ src/pages/my-teams/my-teams.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 
 import { TournamentsPage } from './../tournaments'
+import { TeamHomePage } from './../team-home/'
+import { EliteApiService } from './../../app/shared/'
 
 /*
   Generated class for the MyTeams page.
@@ -15,12 +17,39 @@ import { TournamentsPage } from './../tournaments'
 })
 export class MyTeamsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  private favorites = [
+    {
+      team: { id: 6182, name: 'HC Elite 7th', coach: 'Michelotti'},
+      tournamentId: '89e13aa2-ba6d-4f55-9cc2-61eba6172c63',
+      tournamentName: 'March Madness Tournament'
+    },
+    {
+      team: { id: 805, name: 'HC Elite', coach: 'Michelotti'},
+      tournamentId: '98c6857e-b0d1-4295-b89e-2d95a45437f2',
+      tournamentName: 'Holiday Hopps Challenge'
+    }
+  ];
+
+  constructor(
+    private navCtrl: NavController,
+    private navParams: NavParams,
+    private loading: LoadingController,
+    private eliteApiService: EliteApiService) {}
 
   private goToTournamets() {
     this.navCtrl.push(
       TournamentsPage
     )
+  }
+
+  favoriteTapped($event, favorite): void {
+    let loader = this.loading.create({
+      content: 'Getting data',
+      dismissOnPageChange: true
+    });
+    loader.present();
+    this.eliteApiService.getTournamentsData(favorite.tournamentId)
+      .subscribe( t => this.navCtrl.push(TeamHomePage, favorite.team));
   }
 
 }
